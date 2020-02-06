2d45842b0ec97f76fffc6b79c699cf580300c3ba
Elian Izaguirre <elian_beto@outlook.com>
2020-02-06 08:53:21 -0600

    actualizar

--- Tarea01.R
+++ Tarea01.R
@@ -37,4 +37,23 @@ conjunto$Diametro <= 16.9
 
 #Visualizacion de datos
 
-hist(H.media)
+hist(H.16$Altura, col="blue", xlab = "H.16$Altura (m)",ylab = "Frecuencia", main = "Histograma de H.16"
+hist(Vecinos_3$Vecinos, col="Yellow", xlab = "Vecinos_3",ylab = "Frecuencia", main = "Histograma vecinos_3")
+hist(conjunto$Diametro, col = "red", xlab = "Histograma", ylab = "Frecuencia", main = "Histograma de Diametro")     
+hist(DBH_16$Diametro, col = "green", xlab = "Histograma de DBH_16", ylab = "Frecuencia", main = "Histograma de DBH_16")
+
+ts.dug.h<-subset(conjunto, conjunto$Especie == "H")
+ts.dug.f<-subset(conjunto, conjunto$Especie == "F")
+ts.dug.hf<-subset(conjunto, conjunto$Especie == "H")
+
+HyF<- rbind(ts.dug.h, ts.dug.f)
+
+
+# 3 Graficas
+
+par(mfrow=c(2,2))
+hist(Tsuga_heterofila$Altura)
+hist(Douglasia_verde$Altura)
+hist(Tsuga_heterofila$Diametro)
+hist(Douglasia_verde$Diametro)
+par(mfrow=c(1,1))
